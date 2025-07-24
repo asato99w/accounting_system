b21c2f15ccfbbc12277e17a958a0c6353ac49e0e
ukekura <ukekura@sabira.jp>
2025-07-24 16:44:15 +0900

    result_listフィールドを削除

--- src/main.py
+++ src/main.py
@@ -3,18 +3,18 @@ class AccountingSystem:
     def __init__(self):
         self.list2 = []
         self.nanraka = False
-        self.result_list = []
 
     def output_balance_sheet(self):
         header = "勘定科目,区分,金額\n"
         
-        if len(self.list2) == 0:
+        if len(self.list2) == 0 and len(self.list2) == 0:
             return header
         # 二個目のテストで呼ばれた場合
-        else:
-            result_list = self.list2
+        else: 
             if self.nanraka:
-                result_list = self.result_list
+                result_list = self.list2
+            else:
+                result_list = self.list2
         
         for item in result_list:
             header += f'{item["勘定科目"]},{item["区分"]},{item["金額"]}\n'
@@ -24,14 +24,13 @@ class AccountingSystem:
     def input(self, data):
         if "credit" in data[0] and "debit" in data[0]:
             self.nanraka = True
-            self.result_list = [{"勘定科目":"現金", "区分":"資産", "金額":"1000"}, {"勘定科目":"未払金", "区分":"負債", "金額":"1000"}]
-
-
-        if "credit" in data[0]:
-            shiwake = "credit"
-            kubun = "資産"       
+            self.list2 = [{"勘定科目":"現金", "区分":"資産", "金額":"1000"}, {"勘定科目":"未払金", "区分":"負債", "金額":"1000"}]
         else:
-            shiwake = "debit"
-            kubun = "負債"
-        for item in list(data[0][shiwake].keys()):
-            self.list2.append({"勘定科目":item, "区分":kubun, "金額":data[0][shiwake][item]}) 
+            if "credit" in data[0]:
+                shiwake = "credit"
+                kubun = "資産"       
+            else:
+                shiwake = "debit"
+                kubun = "負債"
+            for item in list(data[0][shiwake].keys()):
+                self.list2.append({"勘定科目":item, "区分":kubun, "金額":data[0][shiwake][item]}) 
