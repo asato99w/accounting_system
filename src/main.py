4c937d7e9f07354c4947099fd7612e0d386b1651
ukekura <ukekura@sabira.jp>
2025-07-24 16:45:33 +0900

    またnanrakaフィールドが消えた

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@
 class AccountingSystem:
     def __init__(self):
         self.list2 = []
-        self.nanraka = False
 
     def output_balance_sheet(self):
         header = "勘定科目,区分,金額\n"
@@ -10,11 +9,8 @@ class AccountingSystem:
         if len(self.list2) == 0 and len(self.list2) == 0:
             return header
         # 二個目のテストで呼ばれた場合
-        else: 
-            if self.nanraka:
-                result_list = self.list2
-            else:
-                result_list = self.list2
+        else:
+            result_list = self.list2
         
         for item in result_list:
             header += f'{item["勘定科目"]},{item["区分"]},{item["金額"]}\n'
@@ -23,7 +19,6 @@ class AccountingSystem:
     
     def input(self, data):
         if "credit" in data[0] and "debit" in data[0]:
-            self.nanraka = True
             self.list2 = [{"勘定科目":"現金", "区分":"資産", "金額":"1000"}, {"勘定科目":"未払金", "区分":"負債", "金額":"1000"}]
         else:
             if "credit" in data[0]:
