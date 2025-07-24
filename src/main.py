2faae1ea984deaf15f5db6b7f431f22a1fc424ca
ukekura <ukekura@sabira.jp>
2025-07-24 13:20:42 +0900

    result_list　引き上げ

--- src/main.py
+++ src/main.py
@@ -7,22 +7,17 @@ class AccountingSystem:
         self.__price = 0
 
     def output_balance_sheet(self):
-        
+        header = "勘定科目,区分,金額\n"
+        result_list = [{"勘定科目":self.__account_item, "区分":self.__kubun, "金額":self.__price}]
         # 一個目のテストで呼ばれた場合
         if self.__condition:
-            
-            result_list = [{"勘定科目":"現金", "区分":"資産", "金額":0}, {"勘定科目":"未払金", "区分":"負債", "金額":0}]
-            header = "勘定科目,区分,金額\n"
-            for item in result_list:
-                header += f'{item["勘定科目"]},{item["区分"]},{item["金額"]}\n'
             return header
         # 二個目のテストで呼ばれた場合
         else:
-            result_list = [{"勘定科目":self.__account_item, "区分":self.__kubun, "金額":self.__price}]
-            header = "勘定科目,区分,金額\n"
             for item in result_list:
                 header += f'{item["勘定科目"]},{item["区分"]},{item["金額"]}\n'
             return header
+        
     
     def input(self, data):
         self.__condition = False
