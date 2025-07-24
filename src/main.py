45138e321e5ab38bdde9a94c7fd13599329973ce
ukekura <ukekura@sabira.jp>
2025-07-24 14:56:49 +0900

    消せるフィールは消してみた

--- src/main.py
+++ src/main.py
@@ -2,16 +2,14 @@
 class AccountingSystem:
     def __init__(self):
         self.__account_item = ""
-        self.__kubun = ""
-        self.__price = 0
         self.nanraka = True
         self.list2 = []
+        self.list1 = []
 
     def output_balance_sheet(self):
         header = "勘定科目,区分,金額\n"
-        list1 = [{"勘定科目":self.__account_item, "区分":self.__kubun, "金額":self.__price}]
         
-        result_list = list1
+        result_list = self.list1
         # 一個目のテストで呼ばれた場合
         if self.__account_item == "":
             return header
@@ -27,6 +25,7 @@ class AccountingSystem:
         
     
     def input(self, data):
+
         if "credit" in data[0]:
 
             if len(list(data[0]["credit"].keys())) > 1:
@@ -37,11 +36,7 @@ class AccountingSystem:
                     self.list2.append({"勘定科目":item, "区分":"資産", "金額":data[0]["credit"][item]})
 
             self.__account_item = list(data[0]["credit"].keys())[0]
-            self.__kubun = "資産"
-            self.__price = data[0]["credit"][self.__account_item]
+            self.list1 = [{"勘定科目":list(data[0]["credit"].keys())[0], "区分":"資産", "金額":data[0]["credit"][list(data[0]["credit"].keys())[0]]}]
         else:
             self.__account_item = list(data[0]["debit"].keys())[0]
-            self.__kubun = "負債"
-            self.__price = data[0]["debit"][self.__account_item]
-        
-    
+            self.list1 = [{"勘定科目":list(data[0]["debit"].keys())[0], "区分":"負債", "金額":data[0]["debit"][list(data[0]["debit"].keys())[0]]}]
