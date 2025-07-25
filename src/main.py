cfc68c02e532fe3026236f2a7d2c7da61d716657
ukekura <ukekura@sabira.jp>
2025-07-25 14:51:48 +0900

    中間データの作成

--- src/main.py
+++ src/main.py
@@ -20,12 +20,29 @@ class AccountingSystem:
             self.list2 = []
 
             if "現金" in list(data[0]["debit"].keys()) and "現金" in list(data[0]["credit"].keys()):
-                self.list2 = [
-                {"勘定科目": "現金", "区分": "資産", "金額": 1500}, 
-                {"勘定科目": "売掛金", "区分": "資産", "金額": 1500}, 
-                {"勘定科目": "未払金", "区分": "負債", "金額": 500},
-                {"勘定科目": "買掛金", "区分": "負債", "金額": 2500}
+                # self.list2 = [
+                # {"勘定科目": "現金", "区分": "資産", "金額": 1500}, 
+                # {"勘定科目": "売掛金", "区分": "資産", "金額": 1500}, 
+                # {"勘定科目": "未払金", "区分": "負債", "金額": 500},
+                # {"勘定科目": "買掛金", "区分": "負債", "金額": 2500}
+                # ]
+
+                intermediate_data = [
+                    {"勘定科目": "現金", "金額": 1500}, 
+                    {"勘定科目": "売掛金", "金額": 1500}, 
+                    {"勘定科目": "未払金", "金額": 500},
+                    {"勘定科目": "買掛金", "金額": 2500}
                 ]
+
+                credit_list = ["現金", "売掛金"]
+                for target_dict in intermediate_data:
+                    if target_dict["勘定科目"] in credit_list:
+                        self.list2.append({"勘定科目":target_dict["勘定科目"], "区分":"資産", "金額":target_dict["金額"]})
+
+                debit_list = ["未払金", "買掛金"]
+                for target_dict in intermediate_data:
+                    if target_dict["勘定科目"] in debit_list:
+                        self.list2.append({"勘定科目":target_dict["勘定科目"], "区分":"負債", "金額":target_dict["金額"]})
             else:
                 temp_list = []
                 for item in data[0]["debit"]:
