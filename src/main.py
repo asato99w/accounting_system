ab01510c3ccc6f4d5b642f0ed7e46f3c556fef02
ukekura <ukekura@sabira.jp>
2025-07-25 17:44:59 +0900

    例外を投げるように仕様を変更

--- src/main.py
+++ src/main.py
@@ -15,54 +15,49 @@ class AccountingSystem:
 
 
     def input(self, data):
-        if "credit" in data[0] and "debit" in data[0]:
+        if not ("credit" in data[0] and "debit" in data[0]):
+            raise ValueError
+        
 
-            self.list2 = []
+        self.list2 = []
 
-          
-            mid_data = []
-            for key in data[0]["debit"]:
-                mid_data.append({"勘定科目": key, "仕分": "debit", "金額": data[0]["debit"][key]})
-            for key in data[0]["credit"]:
-                mid_data.append({"勘定科目": key, "仕分": "credit", "金額": data[0]["credit"][key]})
-            shiwakehyou = {
-                "debit": {
-                    "未払金": 1,
-                    "買掛金": 1,
-                    "現金": -1,
-                    "売掛金": -1
-                },
-                "credit": {
-                    "未払金": -1,
-                    "買掛金": -1,
-                    "現金": 1,
-                    "売掛金": 1
-                }
+        mid_data = []
+        for key in data[0]["debit"]:
+            mid_data.append({"勘定科目": key, "仕分": "debit", "金額": data[0]["debit"][key]})
+        for key in data[0]["credit"]:
+            mid_data.append({"勘定科目": key, "仕分": "credit", "金額": data[0]["credit"][key]})
+
+        shiwakehyou = {
+            "debit": {
+                "未払金": 1,
+                "買掛金": 1,
+                "現金": -1,
+                "売掛金": -1
+            },
+            "credit": {
+                "未払金": -1,
+                "買掛金": -1,
+                "現金": 1,
+                "売掛金": 1
             }
-            zandaka = {}
-            for account_item in shiwakehyou["debit"]:
-                zandaka.update({account_item: 0})
-            for target_dict in mid_data:
-                account_item = target_dict["勘定科目"]
-                zandaka[account_item] += target_dict["金額"] * shiwakehyou[target_dict["仕分"]][account_item]
-            kamoku_list = []
-            for key in zandaka:
-                kamoku_list.append({"勘定科目": key, "金額": zandaka[key]})
-            credit_list = ["現金", "売掛金"]
-            for target_dict in kamoku_list:
-                if target_dict["勘定科目"] in credit_list:
-                    self.list2.append({"勘定科目":target_dict["勘定科目"], "区分":"資産", "金額":target_dict["金額"]})
-            debit_list = ["未払金", "買掛金"]
-            for target_dict in kamoku_list:
-                if target_dict["勘定科目"] in debit_list:
-                    self.list2.append({"勘定科目":target_dict["勘定科目"], "区分":"負債", "金額":target_dict["金額"]})
-           
-        else:
-            if "credit" in data[0]:
-                shiwake = "credit"
-                kubun = "資産"       
-            else:
-                shiwake = "debit"
-                kubun = "負債"
-            for item in list(data[0][shiwake].keys()):
-                self.list2.append({"勘定科目":item, "区分":kubun, "金額":data[0][shiwake][item]}) 
+        }
+
+        zandaka = {}
+        for account_item in shiwakehyou["debit"]:
+            zandaka.update({account_item: 0})
+            
+        for target_dict in mid_data:
+            account_item = target_dict["勘定科目"]
+            zandaka[account_item] += target_dict["金額"] * shiwakehyou[target_dict["仕分"]][account_item]
+        kamoku_list = []
+        for key in zandaka:
+            kamoku_list.append({"勘定科目": key, "金額": zandaka[key]})
+        credit_list = ["現金", "売掛金"]
+        for target_dict in kamoku_list:
+            if target_dict["勘定科目"] in credit_list:
+                self.list2.append({"勘定科目":target_dict["勘定科目"], "区分":"資産", "金額":target_dict["金額"]})
+        debit_list = ["未払金", "買掛金"]
+        for target_dict in kamoku_list:
+            if target_dict["勘定科目"] in debit_list:
+                self.list2.append({"勘定科目":target_dict["勘定科目"], "区分":"負債", "金額":target_dict["金額"]})
+    
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,139 +1,134 @@
+import unittest
 from src.main import AccountingSystem
 
-def test_first():
-    accounting_system = AccountingSystem()
-    result = accounting_system.output_balance_sheet()
-    expected = "勘定科目,区分,金額\n"
-    assert result == expected
+class TestAccountingSystem(unittest.TestCase):
 
-def test_2():
+  def test_first(self):
+      accounting_system = AccountingSystem()
+      result = accounting_system.output_balance_sheet()
+      expected = "勘定科目,区分,金額\n"
+      assert result == expected
+
+  def test_2(self):
+      data = [
+              {
+                "credit": {
+                  "現金": 1000
+                }
+              }
+            ]
+      accounting_system = AccountingSystem()
+      with self.assertRaises(ValueError):
+        accounting_system.input(data)
+
+  def test_3(self):
+      data = [
+              {
+                "debit": {
+                  "未払金": 500,
+                }
+              }
+            ]
+      accounting_system = AccountingSystem()
+      with self.assertRaises(ValueError):
+        accounting_system.input(data)
+
+  def test_accounts_receivable(self):
     data = [
             {
               "credit": {
-                "現金": 1000
+                "現金": 1000,
+                "売掛金": 2000
               }
             }
           ]
     accounting_system = AccountingSystem()
-    accounting_system.input(data)
-    result = accounting_system.output_balance_sheet()
-    expected = "勘定科目,区分,金額\n現金,資産,1000\n"
-    assert result == expected
+    with self.assertRaises(ValueError):
+        accounting_system.input(data)
 
-def test_3():
+  def test_accounts_payable(self):
     data = [
             {
               "debit": {
                 "未払金": 500,
+                "買掛金": 2500,
               }
             }
           ]
     accounting_system = AccountingSystem()
-    accounting_system.input(data)
-    result = accounting_system.output_balance_sheet()
-    expected = "勘定科目,区分,金額\n未払金,負債,500\n"
-    assert result == expected
+    with self.assertRaises(ValueError):
+        accounting_system.input(data)
 
-def test_accounts_receivable():
-  data = [
-          {
-            "credit": {
-              "現金": 1000,
-              "売掛金": 2000
-            }
-          }
-        ]
-  accounting_system = AccountingSystem()
-  accounting_system.input(data)
-  result = accounting_system.output_balance_sheet()
-  expected = "勘定科目,区分,金額\n現金,資産,1000\n売掛金,資産,2000\n"
-  assert result == expected
+  def test_debit_and_credit(self):
+      data = [
+              {
+              "debit": {
+                  "未払金": 1000,
+                },
+                "credit": {
+                  "現金": 1000
+                }
+              }
+            ]
+      accounting_system = AccountingSystem()
+      accounting_system.input(data)
+      result = accounting_system.output_balance_sheet()
+      expected = "勘定科目,区分,金額\n現金,資産,1000\n売掛金,資産,0\n未払金,負債,1000\n買掛金,負債,0\n"
+      assert result == expected
 
-def test_accounts_payable():
-  data = [
-          {
-            "debit": {
-              "未払金": 500,
-              "買掛金": 2500,
-            }
-          }
-        ]
-  accounting_system = AccountingSystem()
-  accounting_system.input(data)
-  result = accounting_system.output_balance_sheet()
-  expected = "勘定科目,区分,金額\n未払金,負債,500\n買掛金,負債,2500\n"
-  assert result == expected
-
-def test_debit_and_credit():
+  def test_multi_debit_and_credit(self):
     data = [
             {
-             "debit": {
-                "未払金": 1000,
+            "debit": {
+                "未払金": 500,
+                "買掛金": 2500,
               },
               "credit": {
-                "現金": 1000
+                "現金": 3000
               }
             }
           ]
     accounting_system = AccountingSystem()
     accounting_system.input(data)
     result = accounting_system.output_balance_sheet()
-    expected = "勘定科目,区分,金額\n現金,資産,1000\n売掛金,資産,0\n未払金,負債,1000\n買掛金,負債,0\n"
+    expected = "勘定科目,区分,金額\n現金,資産,3000\n売掛金,資産,0\n未払金,負債,500\n買掛金,負債,2500\n"
     assert result == expected
 
-def test_multi_debit_and_credit():
-  data = [
-          {
-           "debit": {
-              "未払金": 500,
-              "買掛金": 2500,
-            },
-            "credit": {
-              "現金": 3000
+  def test_multi_debit_and_multi_credit(self):
+    data = [
+            {
+            "debit": {
+                "未払金": 500,
+                "買掛金": 2500,
+              },
+              "credit": {
+                "現金": 1500,
+                "売掛金": 1500
+              }
             }
-          }
-        ]
-  accounting_system = AccountingSystem()
-  accounting_system.input(data)
-  result = accounting_system.output_balance_sheet()
-  expected = "勘定科目,区分,金額\n現金,資産,3000\n売掛金,資産,0\n未払金,負債,500\n買掛金,負債,2500\n"
-  assert result == expected
+          ]
+    accounting_system = AccountingSystem()
+    accounting_system.input(data)
+    result = accounting_system.output_balance_sheet()
+    expected = "勘定科目,区分,金額\n現金,資産,1500\n売掛金,資産,1500\n未払金,負債,500\n買掛金,負債,2500\n"
+    assert result == expected
 
-def test_multi_debit_and_multi_credit():
-  data = [
-          {
-           "debit": {
-              "未払金": 500,
-              "買掛金": 2500,
-            },
-            "credit": {
-              "現金": 1500,
-              "売掛金": 1500
+  def test_debit_cash(self):
+    data = [
+            {
+            "debit": {
+                "未払金": 500,
+                "買掛金": 2500,
+                "現金": 500
+              },
+              "credit": {
+                "現金": 2000,
+                "売掛金": 1500
+              }
             }
-          }
-        ]
-  accounting_system = AccountingSystem()
-  accounting_system.input(data)
-  result = accounting_system.output_balance_sheet()
-  expected = "勘定科目,区分,金額\n現金,資産,1500\n売掛金,資産,1500\n未払金,負債,500\n買掛金,負債,2500\n"
-  assert result == expected
-
-def test_debit_cash():
-  data = [
-          {
-           "debit": {
-              "未払金": 500,
-              "買掛金": 2500,
-              "現金": 500
-            },
-            "credit": {
-              "現金": 2000,
-              "売掛金": 1500
-            }
-          }
-        ]
-  accounting_system = AccountingSystem()
-  accounting_system.input(data)
-  result = accounting_system.output_balance_sheet()
-  expected = "勘定科目,区分,金額\n現金,資産,1500\n売掛金,資産,1500\n未払金,負債,500\n買掛金,負債,2500\n"
-  assert result == expected
+          ]
+    accounting_system = AccountingSystem()
+    accounting_system.input(data)
+    result = accounting_system.output_balance_sheet()
+    expected = "勘定科目,区分,金額\n現金,資産,1500\n売掛金,資産,1500\n未払金,負債,500\n買掛金,負債,2500\n"
+    assert result == expected
