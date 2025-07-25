83ca7ca4e2ed329d38c042fe10ce5afa1db678ae
ukekura <ukekura@sabira.jp>
2025-07-25 13:03:11 +0900

    自主学習すべきところ

--- src/main.py
+++ src/main.py
@@ -22,20 +22,69 @@ class AccountingSystem:
 
 
             self.list2 = []
-            
-        
-            temp_list = [
-                {"勘定科目":"現金", "金額":"1000"},
-                 {"勘定科目":"未払金", "金額":"1000"}
-            ]
 
-            account_item_list = []
-            for account_item in temp_list:
-                account_item_list.append(account_item["勘定科目"])
+            target_list = list(data[0].values())
 
-            if "現金" in account_item_list:
-                self.list2.append({"勘定科目":"現金", "区分":"資産", "金額":"1000"})
-            self.list2.append({"勘定科目":"未払金", "区分":"負債", "金額":"1000"})
+            target_list = [{"未払金": 1000}, {"現金": 1000}]
+
+            new_dict = {}
+            for target_dict in target_list:
+                new_dict.update(target_dict)
+
+            expected_new_dict = {"未払金": 1000, "現金": 1000}
+
+            if new_dict == expected_new_dict:
+
+                keys_list = list(new_dict.keys())
+
+                values_list = list(new_dict.values())
+
+                new_dict_list = []
+                for key in keys_list:
+                    new_dict_list.append({"勘定科目": key})
+
+                new_dict_list = [{"勘定科目": "未払金"}, {"勘定科目": "現金"}]
+
+                # for dict, value in new_dict_list, values_list:
+                #     dict.update({"金額": value})
+
+
+                keys_list = []
+                values_list = []
+                for target_dict in target_list:
+                    keys_list.append(list(target_dict.keys()))
+                    values_list.append(list(target_dict.values()))
+
+                # keys_list = ["未払金", "現金"]
+                # values_list = [1000, 1000]
+
+                new_dict_list = []
+                for key in keys_list:
+                    new_dict_list.append({"勘定科目": key})
+                # for value in values_list:
+                #     new_dict_list.append({"金額": value})
+
+                temp_list = [
+                    {"勘定科目":"未払金", "金額":"1000"},
+                    {"勘定科目":"現金", "金額":"1000"}
+                ]
+
+                # temp_list = new_dict_list
+
+                for target_dict in temp_list:
+                    if "現金" == target_dict["勘定科目"]:
+                        self.list2.append({"勘定科目":"現金", "区分":"資産", "金額":"1000"})
+                for target_dict in temp_list:
+                    if "未払金" == target_dict["勘定科目"]:
+                        self.list2.append({"勘定科目":"未払金", "区分":"負債", "金額":"1000"})
+
+                # account_item_list = []
+                # for account_item in temp_list:
+                #     account_item_list.append(account_item["勘定科目"])
+
+                # if "現金" in account_item_list:
+                #     self.list2.append({"勘定科目":"現金", "区分":"資産", "金額":"1000"})
+                # self.list2.append({"勘定科目":"未払金", "区分":"負債", "金額":"1000"})
             
         else:
             if "credit" in data[0]:
