4f71c43ecf10d4bae66d6692efd1be5ca9aa56e4
ukekura <ukekura@sabira.jp>
2025-07-25 14:10:34 +0900

    expected_listを作成

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ class AccountingSystem:
         header = "勘定科目,区分,金額\n"
         
         if self.something:
+            expected_list = ["現金,資産,1500\n", "売掛金,資産,1500\n", "未払金,負債,500\n", "買掛金,負債,2500\n"]
             expected = "現金,資産,1500\n売掛金,資産,1500\n未払金,負債,500\n買掛金,負債,2500\n"
             header += expected
             return header
