7faf4ea7f2551095bf38a058f025fe072129d5a5
ukekura <ukekura.takuma@gmail.com>
2025-07-26 17:04:25 +0900

    やっとBS最後のテストが通った

--- src/main.py
+++ src/main.py
@@ -27,92 +27,80 @@ class AccountingSystem:
             for key in trade["credit"]:
                 mid_data.append({"勘定科目": key, "仕分": "credit", "金額": trade["credit"][key]})
 
-            motocho = {
-                    "未払金": [],
-                    "買掛金": [],
-                    "現金": [],
-                    "売掛金": [],
-                    "資本金": []
-                }
-
-            for target_dict in mid_data:
-                for key in motocho:
-                    if target_dict["勘定科目"] == key:
-                        motocho[key].append(target_dict)
-
-            shiwakehyou = {
-                "debit": {
-                    "未払金": 1,
-                    "買掛金": 1,
-                    "現金": -1,
-                    "売掛金": -1,
-                    "資本金": 1
-                },
-                "credit": {
-                    "未払金": -1,
-                    "買掛金": -1,
-                    "現金": 1,
-                    "売掛金": 1,
-                    "資本金": -1
-                }
+        motocho = {
+                "未払金": [],
+                "買掛金": [],
+                "現金": [],
+                "売掛金": [],
+                "資本金": []
             }
-
-            shiwakehyou2 = {
-                "debit": {
-                    "未払金": "debit",
-                    "買掛金": "debit",
-                    "現金": "credit",
-                    "売掛金": "credit",
-                    "資本金": "debit"
-                },
-                "credit": {
-                    "未払金": "debit",
-                    "買掛金": "debit",
-                    "現金": "credit",
-                    "売掛金": "credit",
-                    "資本金": "debit"
-                }
-            }
-
-            for account_item in shiwakehyou["debit"]:
-                zandaka.update({account_item: 0})
-
-            # for target_dict in mid_data:
-            #     account_item = target_dict["勘定科目"]
-            #     zandaka[account_item] += target_dict["金額"] * shiwakehyou[target_dict["仕分"]][account_item]
-
+        for target_dict in mid_data:
             for key in motocho:
-                nanika = 0
-                nanika2 = 0
-                for target_dict in motocho[key]:
-                    if target_dict["仕分"] == "debit":
-                        nanika += target_dict["金額"]
-                    else:
-                        nanika2 += target_dict["金額"]
-
-                if nanika - nanika2 > 0:
-                    shiwake = "debit"
-                    kingaku = nanika - nanika2
+                if target_dict["勘定科目"] == key:
+                    motocho[key].append(target_dict)
+        shiwakehyou = {
+            "debit": {
+                "未払金": 1,
+                "買掛金": 1,
+                "現金": -1,
+                "売掛金": -1,
+                "資本金": 1
+            },
+            "credit": {
+                "未払金": -1,
+                "買掛金": -1,
+                "現金": 1,
+                "売掛金": 1,
+                "資本金": -1
+            }
+        }
+        shiwakehyou2 = {
+            "debit": {
+                "未払金": "debit",
+                "買掛金": "debit",
+                "現金": "credit",
+                "売掛金": "credit",
+                "資本金": "debit"
+            },
+            "credit": {
+                "未払金": "debit",
+                "買掛金": "debit",
+                "現金": "credit",
+                "売掛金": "credit",
+                "資本金": "debit"
+            }
+        }
+        for account_item in shiwakehyou["debit"]:
+            zandaka.update({account_item: 0})
+        # for target_dict in mid_data:
+        #     account_item = target_dict["勘定科目"]
+        #     zandaka[account_item] += target_dict["金額"] * shiwakehyou[target_dict["仕分"]][account_item]
+        for key in motocho:
+            nanika = 0
+            nanika2 = 0
+            for target_dict in motocho[key]:
+                if target_dict["仕分"] == "debit":
+                    nanika += target_dict["金額"]
                 else:
-                    shiwake = "credit"
-                    kingaku = nanika2 - nanika
-
-                zandaka[key] = [kingaku, shiwake]
-
-            # for item in zandaka:
-            #     zandaka[item] = [zandaka[item], shiwakehyou2["credit"][item]]
-
-            for key in zandaka:
-                kamoku_list.append({"勘定科目": key, "金額": zandaka[key]})
-
-            # kamoku_list = [
-            #     {"勘定科目": "未払金", "金額": [500, "debit"]},
-            #     {"勘定科目": "買掛金", "金額": [2500, "debit"]},
-            #     {"勘定科目": "現金", "金額": [1500, "credit"]},
-            #     {"勘定科目": "売掛金", "金額": [1500, "credit"]},
-            #     {"勘定科目": "資本金", "金額": [0, "debit"]}
-            # ]
-
+                    nanika2 += target_dict["金額"]
+            if nanika - nanika2 > 0:
+                shiwake = "debit"
+                kingaku = nanika - nanika2
+            else:
+                shiwake = "credit"
+                kingaku = nanika2 - nanika
+            zandaka[key] = [kingaku, shiwake]
+        # for item in zandaka:
+        #     zandaka[item] = [zandaka[item], shiwakehyou2["credit"][item]]
+        for key in zandaka:
+            kamoku_list.append({"勘定科目": key, "金額": zandaka[key]})
+        # kamoku_list = [
+        #     {"勘定科目": "未払金", "金額": [500, "debit"]},
+        #     {"勘定科目": "買掛金", "金額": [2500, "debit"]},
+        #     {"勘定科目": "現金", "金額": [1500, "credit"]},
+        #     {"勘定科目": "売掛金", "金額": [1500, "credit"]},
+        #     {"勘定科目": "資本金", "金額": [0, "debit"]}
+        # ]
 
 
         credit_list = ["現金", "売掛金"]
