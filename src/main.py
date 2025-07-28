b2e9db91df40b01fd732815594e8a1b61dbd5984
asato <asato@sabira.jp>
2025-07-28 16:46:49 +0900

    損益計算書クラスの作成

--- src/main.py
+++ src/main.py
@@ -92,27 +92,8 @@ class AccountingSystem:
         for kamokumei in kamokugoto_zandaka_dict:
             dict_of_kamoku_and_kingaku_list.append({"勘定科目": kamokumei, "金額": kamokugoto_zandaka_dict[kamokumei]})
 
-
-        # 損益計算書の計算
-        line_items = []
-        profit_and_expense_account_types = [Revenue(), Expense()]
-        for account_type in profit_and_expense_account_types:
-            profit_and_expense_content = account_type.create_statement(dict_of_kamoku_and_kingaku_list)
-            line_items.extend(profit_and_expense_content)
-
-        net_income = 0
-        for item in line_items:
-            if item["区分"] == "収益":
-                net_income += item["金額"]
-            elif item["区分"] == "費用":
-                net_income -= item["金額"]
-
-        if net_income > 0:
-            line_items.append({"勘定科目": "当期純利益", "区分": "純利益", "金額": net_income})
-        elif net_income < 0:
-            line_items.append({"勘定科目": "当期純損失", "区分": "純損失", "金額": net_income * -1})
-
-        self.__pl_content_dict_list = line_items
+        pl = ProfitAndLoss(dict_of_kamoku_and_kingaku_list)
+        self.__pl_content_dict_list = pl.get_line_items()
 
         # 貸借対照表の計算
         line_items = []
@@ -121,9 +102,59 @@ class AccountingSystem:
             balance_sheet_content = account_type.create_statement(dict_of_kamoku_and_kingaku_list)
             line_items.extend(balance_sheet_content)
 
+        net_income = pl.get_net_income()
+
         if net_income > 0:
             line_items.append({"勘定科目": "利益剰余金", "区分": "純資産", "金額": net_income})
         elif net_income < 0:
             line_items.append({"勘定科目": "利益剰余金", "区分": "純資産", "金額": net_income})
 
         self.__kamoku_kubun_kingaku_list = line_items
+
+
+class ProfitAndLoss:
+
+    def __init__(self, worksheet):
+        self.__profit_and_expense_account_types = [Revenue(), Expense()]
+
+        self.__line_items_without_net_income = self.__create_line_items_without_net_income(worksheet)
+        self.__net_income = self.__calculate_net_income(self.__line_items_without_net_income)
+
+        self.__line_items = self.__line_items_without_net_income + self.__create_net_income_line(self.__net_income)
+
+    def __create_line_items_without_net_income(self, worksheet):
+        line_items = []
+        for account_type in self.__profit_and_expense_account_types:
+            profit_and_expense_content = account_type.create_statement(worksheet)
+            line_items.extend(profit_and_expense_content)
+
+        return line_items
+
+    def __calculate_net_income(self, line_items):
+        net_income = 0
+        for item in line_items:
+            if item["区分"] == "収益":
+                net_income += item["金額"]
+            elif item["区分"] == "費用":
+                net_income -= item["金額"]
+        return net_income
+
+    def __create_net_income_line(self, net_income):
+        if self.is_profit():
+            return [{"勘定科目": "当期純利益", "区分": "純利益", "金額": net_income}]
+        elif self.is_loss():
+            return [{"勘定科目": "当期純損失", "区分": "純損失", "金額": net_income * -1}]
+        else:
+            return []
+
+    def is_profit(self):
+        return self.__net_income > 0
+
+    def is_loss(self):
+        return self.__net_income < 0
+
+    def get_line_items(self):
+        return self.__line_items
+
+    def get_net_income(self):
+        return self.__net_income
