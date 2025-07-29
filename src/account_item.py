5913c6837c4fe7b4b91b619e8e4d0e07114ec140
KAZU ASATO <kazuasato@example.com>
2025-07-29 20:29:55 +0900

    ファイルを分離

---
+++ src/account_item.py
@@ -0,0 +1,43 @@
+from .account_type import AccountType, Asset, Liability, Equity, Revenue, Expense
+
+class AccountItems:
+    def __init__(self):
+        self.account_item_names = ["未払金", "買掛金", "現金", "売掛金", "資本金", "売上", "受取利息", "給料", "仕入", "地代家賃", "水道光熱費"]
+        self.__account_items = []
+        for account_item_name in self.account_item_names:
+            self.__account_items.append(self.create_account_item(account_item_name))
+
+    def create_account_item(self, name):
+        for account_name in Asset.accounts:
+            if account_name == name:
+                return AccountItem(name, Asset())
+        for account_name in Liability.accounts:
+            if account_name == name:
+                return AccountItem(name, Liability())
+        for account_name in Equity.accounts:
+            if account_name == name:
+                return AccountItem(name, Equity())
+        for account_name in Revenue.accounts:
+            if account_name == name:
+                return AccountItem(name, Revenue(skip_zero=True))
+        for account_name in Expense.accounts:
+            if account_name == name:
+                return AccountItem(name, Expense(skip_zero=True))
+        raise ValueError
+
+    def get_items(self):
+        return self.__account_items
+
+    def get_item(self, name):
+        for account_item in self.__account_items:
+            if account_item.get_name() == name:
+                return account_item
+        raise ValueError
+
+class AccountItem:
+    def __init__(self, name, account_type):
+        self.__name = name
+        self.__account_type = account_type
+
+    def get_name(self):
+        return self.__name
