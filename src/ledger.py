e167b03f7f1fe9d796efc3390bad61ca6036b446
asato <asato@sabira.jp>
2025-07-29 14:02:18 +0900

    ledgerAccountクラスを作成

--- src/ledger.py
+++ src/ledger.py
@@ -1,12 +1,14 @@
 class GeneralLedger:
     def __init__(self, ledgers):
-        self.__ledgers = ledgers
+        self.__ledgers= []
+        for account_item in ledgers:
+            self.__ledgers.append(LedgerAccount({"勘定科目": account_item, "記入内容": ledgers[account_item]}))
 
     def create_trial_balance(self):
         balances = {}
 
-        for account_item in self.__ledgers:
-            balances[account_item] = self.__calculate_amount(account_item)
+        for ledger in self.__ledgers:
+            balances[ledger.get_account_item()] = self.__calculate_amount(ledger)
 
         result = []
         for account_item in balances:
@@ -14,10 +16,10 @@ class GeneralLedger:
 
         return result
 
-    def __calculate_amount(self, account_item):
+    def __calculate_amount(self, ledger):
         credit_amount = 0
         debit_amount = 0
-        for entry in self.__ledgers[account_item]:
+        for entry in ledger.get_entries():
             if entry["仕分"] == "debit":
                 debit_amount += entry["金額"]
             else:
@@ -30,3 +32,15 @@ class GeneralLedger:
             side = "credit"
             amount = credit_amount - debit_amount
         return [amount, side]
+
+class LedgerAccount:
+    def __init__(self, ledger):
+        self.ledger = ledger
+        self.__account_item = ledger["勘定科目"]
+        self.__entries = ledger["記入内容"]
+
+    def get_account_item(self):
+        return self.__account_item
+
+    def get_entries(self):
+        return self.__entries
