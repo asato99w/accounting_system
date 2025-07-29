e66535c6d903b6dbc2545bdff2c97da6d9e36c06
asato <asato@sabira.jp>
2025-07-29 14:13:40 +0900

    責務をledger accountに移す

--- src/ledger.py
+++ src/ledger.py
@@ -8,30 +8,13 @@ class GeneralLedger:
         balances = {}
 
         for ledger in self.__ledgers:
-            balances[ledger.get_account_item()] = self.__calculate_amount(ledger)
+            balances[ledger.get_account_item()] = ledger.get_amount()
 
         result = []
         for account_item in balances:
             result.append({"勘定科目": account_item, "残高": balances[account_item]})
 
         return result
-
-    def __calculate_amount(self, ledger):
-        credit_amount = 0
-        debit_amount = 0
-        for entry in ledger.get_entries():
-            if entry["仕分"] == "debit":
-                debit_amount += entry["金額"]
-            else:
-                credit_amount += entry["金額"]
-
-        if debit_amount - credit_amount > 0:
-            side = "debit"
-            amount = debit_amount - credit_amount
-        else:
-            side = "credit"
-            amount = credit_amount - debit_amount
-        return [amount, side]
 
 class LedgerAccount:
     def __init__(self, ledger):
@@ -44,3 +27,21 @@ class LedgerAccount:
 
     def get_entries(self):
         return self.__entries
+
+    def get_amount(self):
+        debit_amount = self.__calculate_debit_amount()
+        credit_amount = self.__calculate_credit_amount()
+
+        if debit_amount - credit_amount > 0:
+            side = "debit"
+            amount = debit_amount - credit_amount
+        else:
+            side = "credit"
+            amount = credit_amount - debit_amount
+        return [amount, side]
+
+    def __calculate_debit_amount(self):
+        return sum(entry["金額"] for entry in self.__entries if entry["仕分"] == "debit")
+
+    def __calculate_credit_amount(self):
+        return sum(entry["金額"] for entry in self.__entries if entry["仕分"] == "credit")
